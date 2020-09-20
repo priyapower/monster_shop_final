ffee684e95ae77bcc4ec4bc6edcda795a0783b4f
Priya Power <priyajag72@gmail.com>
2020-09-20 15:27:06 -0600

    Add discount database

---
+++ db/migrate/20200920212150_create_discount.rb
@@ -0,0 +1,13 @@
+class CreateDiscount < ActiveRecord::Migration[5.2]
+  def change
+    create_table :discounts do |t|
+      t.string :description
+      t.integer :quantity
+      t.integer :percent
+      t.boolean :enabled, default: true
+      t.references :merchant, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
