30d98967b10361e0565f8e839792bc6b71396c7c
Priya Power <priyajag72@gmail.com>
2020-09-22 16:37:41 -0600

    Add test for us13 - carts automatically update for discounts with a new saved from discounts sectino

---
+++ spec/features/cart/discount_spec.rb
@@ -0,0 +1,54 @@
+require 'rails_helper'
+
+RSpec.describe 'Discounted Cart Show Page' do
+  describe 'As a non-admin user' do
+    before :each do
+      @merchant_1 = Merchant.create!(name: 'Megans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
+      @merchant_2 = Merchant.create!(name: 'Brians Bagels', address: '125 Main St', city: 'Denver', state: 'CO', zip: 80218)
+      @ogre = @merchant_1.items.create!(name: 'Ogre', description: "I'm an Ogre!", price: 500, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 500 )
+      @giant = @merchant_1.items.create!(name: 'Giant', description: "I'm a Giant!", price: 650, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 300 )
+      @hippo = @merchant_2.items.create!(name: 'Hippo', description: "I'm a Hippo!", price: 1500, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 200 )
+
+      @merchant_1_discount_1 = @merchant_1.discounts.create!(description:"Buy 5 items, get 20% off", quantity:5, percent:20)
+      @merchant_1_discount_2 = @merchant_1.discounts.create!(description:"Buy 15 items, get 40% off", quantity:15, percent:40)
+      @merchant_1_discount_3 = @merchant_1.discounts.create!(description:"Buy 30 items, get 60% off", quantity:30, percent:60)
+
+      @merchant_2_discount_1 = @merchant_2.discounts.create!(description:"Buy 2 items, get 5% off", quantity:2, percent:5)
+      @merchant_2_discount_2 = @merchant_2.discounts.create!(description:"Buy 10 items, get 25% off", quantity:10, percent:25)
+
+      4.times do
+        visit item_path(@ogre)
+        click_button 'Add to Cart'
+      end
+
+      visit item_path(@hippo)
+      click_button 'Add to Cart'
+    end
+
+    it "can see updated totals in cart when a discount is met" do
+      visit '/cart'
+
+      within "#item-#{@ogre.id}" do
+        expect(page).to have_content("Quantity: 4")
+      end
+
+      within "#item-#{@hippo.id}" do
+        expect(page).to have_content("Quantity: 1")
+      end
+
+      expect(page).to have_content("Total: $3,500.00")
+
+      within "#item-#{@hippo.id}" do
+        click_button('More of This!')
+      end
+
+      within "#item-#{@hippo.id}" do
+        expect(page).to have_content("Quantity: 2")
+      end
+
+      expect(page).to_not have_content("Total: $5,000.00")
+      expect(page).to have_content("Total: $4,850.00")
+      expect(page).to have_content("Saved from Discounts: $150.00")
+    end
+  end
+end
