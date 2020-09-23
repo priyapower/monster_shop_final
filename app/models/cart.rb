282c489bc50d7c9d1ba84bc027cc93ae5cd89f10
Priya Power <priyajag72@gmail.com>
2020-09-23 06:56:06 -0600

    Add brainstorm to cart PORO for refactor of discount behaviors

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -46,6 +46,35 @@ class Cart
   end
 
   def grand_total
+    grand_total = 0.0
+    @contents.each do |item_id, quantity|
+      grand_total += Item.find(item_id).price * quantity
+    end
+    grand_total
+  end
+
+  # BRAINSTORM FOR REFACTOR
+  # - grand total can still be hit
+  # - Should i put the condition in grand total or in the view?
+      # - If I put it in the view, it needs to compare to something?
+      # I have access to:
+          # Discount conditions met (checks if any meet quantity and returns true)
+              # HOW CAN I MAKE THIS ACTIVERECORD??
+              # meets quantity is a condition == .where("quantity >= discount.quantity")
+              # returns true... The only AR I found that returns a boolean is .exist?
+                  # Does it still need to be a boolean? If I call the record that meets the quantity
+                  # How do I limit it so it also meets the discount
+                      # Can I use a proc to return a hash with the discounted prices?
+                      # If I am able to use .where to check condition AND then use.... .sum? but I still want to call .limit?
+                      # Hmmm....
+                      # Let's first start with removing the discount conditions by using a .where!
+          # All possible discounts (checks all qualifying discount prices and returns the correct discount)
+              # HOW CAN I MAKE THIS ACTIVERECORD??
+          # Update Cart with Discount (does the math behind a discount and returns discounted total for that item/quantity in the cart)
+              # HOW CAN I MAKE THIS ACTIVERECORD??
+
+
+  def grand_total
     @saved_discounts = 0.0
     grand_total = 0.0
     @contents.each do |item_id, quantity|
