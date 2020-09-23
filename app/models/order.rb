813da198b988cc490e748ccacc39d14425459fcc
Priya Power <priyajag72@gmail.com>
2020-09-22 21:24:09 -0600

    Update grand total method from order model for logic about discounts - NEEDS REFACTOR

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,7 +6,28 @@ class Order < ApplicationRecord
   enum status: ['pending', 'packaged', 'shipped', 'cancelled']
 
   def grand_total
-    order_items.sum('price * quantity')
+    grand_total = 0.0
+    hash_to_check_discounts = Hash.new
+    order_items.pluck(:item_id).each do |item_id|
+      item = Item.find(item_id)
+      quantity = order_items.where(item:item).pluck(:quantity)[0]
+      discounts = item.merchant.discounts
+
+      if !discounts.empty?
+        discounts.each do |discount|
+          multiplier = (100 - discount.percent).to_f / 100
+          current_total = item.price * quantity
+          discounted_total = current_total * multiplier
+          hash_to_check_discounts[discount.id] = current_total - discounted_total
+        end
+        discount = Discount.find(hash_to_check_discounts.key(hash_to_check_discounts.values.max))
+        multiplier = (100 - discount.percent).to_f / 100
+        grand_total +=(item.price * quantity) * multiplier
+      else
+        grand_total += item.price * quantity
+      end
+    end
+    grand_total
   end
 
   def count_of_items
